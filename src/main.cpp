abba160ec0049e5630073f87a7645394a353ddfd
Ged <gedi.jerene@gmail.com>
2025-07-22 22:52:04 +0200

    Library compatible with Arduino Core and Compiling

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,23 @@
 #include <Arduino.h>
+#include <ld2410.h>
 
+LD2410 ld2410;
 
 void setup()
 {
+  Serial.begin(256000);
+  Serial1.begin(9600); // Debug Serial on NodeMCU
   
+  ld2410.setBaudrate(256000);
 }
-void loop
+void loop()
 {
   const int max_line_length = 80;
   static char buffer[max_line_length];
-  while (available()) // Read serial
+
+  while (Serial.available()) // Read serial
   {
-    readline(read(), buffer, max_line_length);
+    ld2410.readline(Serial.read(), buffer, max_line_length);
+    Serial1.println(ld2410.hasTarget);
   }
 }
